7f1bf16f02891c2fdf99716f70f07cd0e371bcc1
huguangnan <huguangnan@songxiaocai.com>
2019-11-18 21:36:34 +0800

    feat: tabel

--- pages/home.js
+++ pages/home.js
@@ -1,70 +1,73 @@
 import React, { PureComponent } from "react";
-import { Table, Button, Input, Row, Col, InputNumber } from "antd";
+import { Table, Button, Icon, Input, Row, Col, InputNumber } from "antd";
+
+import styles from "./home.css";
 
 export default class Home extends PureComponent {
   state = {
-    dataSource: [
-    ]
+    a: [], // [{ label: '', value: null }]
+    b: []
   };
 
-  add = (key) => () => {
-    const { dataSource } = this.state
-    dataSource.map((item) => {
-      const res = {...item}
-      // if item[key]
-    })
-  }
+  handleChangeA = (isLabel, index, value) => {
+    const { a } = this.state;
+    const _a = [...a];
+    _a[index][isLabel ? "label" : "value"] = value;
+    this.setState({ a: _a });
+  };
 
-  columns = [
-    {
-      title: "总计",
-      dataIndex: "total"
-    },
-    {
-      title: (
-        <>
-          A <Add onClick={this.add('a')} />
-        </>
-      ),
-      dataIndex: "a",
-      render: ({ a: { ref } = {} }) => {
-        return ref && <Item ref={ref} />;
-      }
-    },
-    {
-      title: (
-        <>
-          B <Add />
-        </>
-      ),
-      dataIndex: "b",
-      render: ({ a: { ref } = {} }) => {
-        return ref && <Item ref={ref} />;
-      }
-    },
-    {
-      title: (
-        <>
-          C <Add />
-        </>
-      ),
-      dataIndex: "c",
-      render: ({ a: { ref } = {} }) => {
-        return ref && <Item ref={ref} />;
-      }
-    }
-  ];
+  addA = () => {
+    const { a } = this.state;
+    this.setState({ a: [...a, { label: "", value: 0 }] });
+  };
 
   render() {
-    const { dataSource } = this.state;
+    window.scope = this;
+    const { a } = this.state;
 
     return (
       <>
-        <Table
-          dataSource={dataSource}
-          columns={this.columns}
-          pagination={false}
-        />
+        <table border="1px solid #666" className={styles["table-wrapper"]}>
+          <tr>
+            <th rowSpan="2" align="center" className={styles["nowrap"]}>
+              BKK-A
+            </th>
+            <th className={styles["nowrap"]}>面料成本</th>
+            <th className={styles["nowrap"]}>中转费、派费成本</th>
+          </tr>
+          <tr>
+            <td style={{ padding: 0 }}>
+              <Line left="面料类型" right="数量" />
+              {a.map(({ label, value }, index) => (
+                <Line
+                  key={index}
+                  left={
+                    <Input
+                      placeholder="label"
+                      value={label}
+                      onChange={event =>
+                        this.handleChangeA(true, index, event.target.value)
+                      }
+                    />
+                  }
+                  right={
+                    <InputNumber
+                      style={{ width: "100%" }}
+                      placeholder="content"
+                      min={0}
+                      value={value}
+                      onChange={value =>
+                        this.handleChangeA(false, index, value)
+                      }
+                    />
+                  }
+                />
+              ))}
+              <Add onClick={this.addA} />
+            </td>
+            <td>33</td>
+          </tr>
+        </table>
       </>
     );
   }
@@ -72,25 +75,36 @@ export default class Home extends PureComponent {
 
 function Add(props) {
   return (
-    <Button size="small" style={{ marginLeft: "5px" }} {...props}>
-      添加
-    </Button>
+    <div className={styles["add-btn"]} {...props}>
+      <Icon type="plus" />
+    </div>
   );
 }
 
 function Item({ ref }) {
   return (
-    <Row gutter={10}>
-      <Col span={12}>
-        <Input placeholder="label" ref={ref => this[`${ref}_label`]} />
-      </Col>
-      <Col span={12}>
+    <Line
+      left={<Input placeholder="label" ref={ref => this[`${ref}_label`]} />}
+      right={
         <InputNumber
           ref={ref => this[`${ref}_content`]}
           style={{ width: "100%" }}
           placeholder="content"
           min={0}
         />
+      }
+    />
+  );
+}
+
+function Line({ left, right }) {
+  return (
+    <Row className={styles["line"]}>
+      <Col span={12}>
+        <div className={styles["left"]}>{left}</div>
+      </Col>
+      <Col span={12}>
+        <div className={styles["right"]}>{right}</div>
       </Col>
     </Row>
   );
